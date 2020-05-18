fb5e498c440015b6198d4b92e61ca7a804aa13c1
2133069 <2133069@slu.edu.ph>
2020-05-18 21:32:03 +0800

    Pushed

--- Sample.java
+++ Sample.java
@@ -1,9 +1,5 @@
 public Sample{
     public static void main(String[]args){
         System.out.print("This is a sample.");
-<<<<<<< HEAD
-        
-=======
->>>>>>> parent of 2841a27... Update Sample.java
     }
 }
