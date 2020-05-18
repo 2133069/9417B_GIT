226358680ea11428177e859f3876f121473b0639
2133069 <2133069@slu.edu.ph>
2020-05-18 21:36:49 +0800

    reverting

--- Sample.java
+++ Sample.java
@@ -1,6 +1,9 @@
 public Sample{
     public static void main(String[]args){
         System.out.print("This is a sample.");
+<<<<<<< HEAD
         System.out.print("This is a edited using GitHub.");
+=======
+>>>>>>> parent of 2841a27... Update Sample.java
     }
 }
